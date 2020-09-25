bd1c393e06c26314403215e9729199eac368c2dc
Hans Wennborg <hans@chromium.org>
2020-09-25 19:49:15 +0200

    Move PassBuilder::registerParseTopLevelPipelineCallback out-of-line
    For some mysterious reason it doesn't build with clang-cl when compiled
    as part of the includes in clang's CodeGenAction.cpp
    (crbug.com/1132292).

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -2836,3 +2836,9 @@ bool PassBuilder::isAnalysisPassName(StringRef PassName) {
 #include "PassRegistry.def"
   return false;
 }
+
+void PassBuilder::registerParseTopLevelPipelineCallback(
+    const std::function<bool(ModulePassManager &, ArrayRef<PipelineElement>,
+                             bool VerifyEachPass, bool DebugLogging)> &C) {
+  TopLevelPipelineParsingCallbacks.push_back(C);
+}
